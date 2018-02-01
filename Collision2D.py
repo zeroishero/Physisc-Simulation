eadb0e5d2abf4cac2319a620c2f4e3a370897167
Sakar Dotel <sakardotel14@gmail.com>
2018-02-02 00:57:24 +0545

    Collision file except input screen added, 2 dimensional collision file to be completed

--- Collision2D.py
+++ Collision2D.py
@@ -4,27 +4,69 @@ import pygame
 import sys
 
 
-def mouse_button(screen, x1, y1, x2, y2, r1, r2):
+class Circle:
+    def __init__(self, screen, r, x, y):
+        self.screen = screen
+        self.radius = r
+        self.pos = (x, y)
+
+    def draw_circle(self):
+        pygame.draw.circle(self.screen, (255, 0, 0), self.pos, self.radius, 0)
+
+
+def draw_circle(screen, x1, y1, x2, y2, r1, r2):  # Function for drawing the balls and borders on the screen
+    screen.fill((255, 255, 255))
+    ElasticCollision.border(screen)
+    pygame.draw.circle(screen, (255, 0, 0), (int(x1), int(y1)), r1, 0)
+    pygame.draw.circle(screen, (255, 0, 0), (int(x2), int(y2)), r2, 0)
+    pygame.display.flip()
+
+
+def plot_circle(circles_plotted):
+    pos = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            circles_plotted += 1
+    ElasticCollision.event_handle()
+    return pos, circles_plotted
+
+
+# Function for the movement of the object using mouse and event handling
+'''def mouse_move_object(screen, x1, y1, x2, y2, r1, r2):
     pos1 = (x1, y1)
     pos2 = (x2, y2)
     moving = True
     for event in pygame.event.get():
+        # Getting the initial position of the mouse cursor for checking the remaining conditions
         initial_pos = pygame.mouse.get_pos()
         initial_x = initial_pos[0]
         initial_y = initial_pos[1]
-        if event.type == pygame.MOUSEBUTTONDOWN and (x1+r1) > initial_x > (x1-r1) and (y1+r1) > initial_y > (y1-r1):
-            while moving:
-                for sub_event in pygame.event.get():
-                    if sub_event.type == pygame.MOUSEMOTION:
-                        pos = pygame.mouse.get_pos()
-                        screen.fill((255, 255, 255))
-                        ElasticCollision.border(screen)
-                        pygame.draw.circle(screen, (255, 0, 0), pos, r1, 0)
-                        pygame.draw.circle(screen, (255, 0, 0), pos, r2, 0)
-                        pygame.display.flip()
-                        pygame.display.update()
-                    if sub_event.type == pygame.MOUSEBUTTONUP:
-                        moving = False
+        # Checking whether the mouse cursor is inside the first ball or not
+        if (x1 + r1) > initial_x > (x1 - r1) and (y1 + r1) > initial_y > (y1 - r1):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                while moving:  # Loop for the movement of the ball until the mouse button is pressed
+                    for sub_event in pygame.event.get():
+                        if sub_event.type == pygame.MOUSEMOTION:
+                            pos1 = pygame.mouse.get_pos()
+                            x1 = pos1[0]
+                            y1 = pos1[1]
+                            draw_circle(screen, x1, y1, x2, y2, r1, r2)
+                        if sub_event.type == pygame.MOUSEBUTTONUP:
+                            moving = False
+        # Checking whether the mouse cursor is inside the second ball or not
+        elif (x2 + r2) > initial_x > (x2 - r2) and (y2 + r2) > initial_y > (y2 - r2):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                while moving:  # Loop for the movement of the ball until the mouse button is pressed
+                    for sub_event in pygame.event.get():
+                        if sub_event.type == pygame.MOUSEMOTION:
+                            pos2 = pygame.mouse.get_pos()
+                            x2 = pos2[0]
+                            y2 = pos2[1]
+                            draw_circle(screen, x1, y1, x2, y2, r1, r2)
+                        if sub_event.type == pygame.MOUSEBUTTONUP:
+                            moving = False
+        # Event handling conditions as per the requirement
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
@@ -32,34 +74,61 @@ def mouse_button(screen, x1, y1, x2, y2, r1, r2):
                 ElasticCollision.pause_program()
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
-    return pos1, pos2
+    return pos1, pos2'''
 
 
-def collision2d():  # Function for the main program loop
+def main_program():  # Function for the main program loop
     pygame.init()
     screen = pygame.display.set_mode((1366, 768))
     pygame.display.set_caption('Physics Simulation')
     pygame.display.flip()
-    done = 0
+    done = False
+    plotted = False
     x1 = 200
     y1 = 384
     x2 = 1000
     y2 = 384
     r1 = 50
     r2 = 50
+    circle_made = 0
+    count = 0
     while not done:  # Infinite program loop
         screen.fill((255, 255, 255))
         ElasticCollision.border(screen)
-        # ElasticCollision.display_value()
-        pygame.draw.circle(screen, (255, 0, 0), (int(x1), int(y1)), r1, 0)
-        pygame.draw.circle(screen, (255, 0, 0), (int(x2), int(y2)), r2, 0)
-        pos = mouse_button(screen, x1, y1, x2, y2, r1, r2)
-        x1 = pos[0]
-        y1 = pos[1]
+        # display_value()
+        ElasticCollision.event_handle()
+        while not plotted:
+            ElasticCollision.event_handle()
+            pygame.display.flip()
+            pos = plot_circle(circle_made)
+            if pos[1] == 1 and count == 0:
+                x1 = pos[0][0]
+                y1 = pos[0][1]
+                count += 1
+                pygame.draw.circle(screen, (255, 0, 0), (x1, y1), r1, 0)
+            if pos[1] == 2:
+                x2 = pos[0][0]
+                y2 = pos[0][1]
+                pygame.draw.circle(screen, (255, 0, 0), (x2, y2), r2, 0)
+                plotted = True
+            circle_made = pos[1]
+        circle1 = Circle(screen, r1, int(x1), int(y1))
+        circle2 = Circle(screen, r2, int(x2), int(y2))
+        circle1.draw_circle()
+        circle2.draw_circle()
+        move
+        '''x1 = pos1[0]
+        x2 = pos2[0]
+        y1 = pos1[1]
+        y2 = pos2[1]'''
         pygame.display.flip()
-        pygame.display.update()
 
 
-collision2d()
-pygame.quit()
-quit()
+def collision2d():
+    main_program()
+    pygame.quit()
+    quit()
+
+
+if __name__ == '__main__':
+    collision2d()
--- ElasticCollision.py
+++ ElasticCollision.py
@@ -8,9 +8,33 @@ def border(screen):
     pygame.draw.line(screen, [0, 0, 0], (10, 10), (10, 758), 4)
     pygame.draw.line(screen, [0, 0, 0], (1356, 10), (1356, 758), 4)
     pygame.draw.line(screen, [0, 0, 0], (10, 758), (1356, 758), 4)
+    # For creating the box for displaying the values while simulation
+    pygame.draw.line(screen, [0, 0, 0], (950, 25), (1335, 25), 3)
+    pygame.draw.line(screen, [0, 0, 0], (950, 25), (950, 120), 3)
+    pygame.draw.line(screen, [0, 0, 0], (950, 120), (1335, 120), 3)
+    pygame.draw.line(screen, [0, 0, 0], (1335, 25), (1335, 120), 3)
 
 
-# def display_value():
+class DisplayDetails:  # Class for displaying the message while rendering the required values
+    def __init__(self, screen, pos, message):  # Variable initialization function
+        self.font = pygame.font.SysFont("Courier", 18, bold=False)
+        self.screen_text = self.font.render(message, True, (0, 0, 255))
+        self.location = pos
+        self.screen = screen
+
+    def display_details(self):  # Function for displaying the values
+        self.screen.blit(self.screen_text, self.location)
+
+
+class DisplayValues:  # Class for displaying the required values of the variables
+    def __init__(self, screen, pos, value):  # Function for variable initialization
+        self.font = pygame.font.SysFont("Courier", 18, bold=False)
+        self.screen = screen
+        self.screen_value = self.font.render(value, True, (0, 0, 255))
+        self.location = pos
+
+    def display_value(self):  # Function for rendering the values in variables
+        self.screen.blit(self.screen_value, self.location)
 
 
 def pause_program():  # Function for pausing the program
@@ -49,35 +73,36 @@ def size_obj(m1, m2):  # Function for determining which of the two objects is bi
     return r1, r2
 
 
-def has_collided(x1, x2, r1, r2, u1, u2, m1, m2, collision_type):  # Function for checking collision and calculating velocity
+# Function for checking whether collision has taken place and calculating velocity after collision
+def has_collided(x1, x2, r1, r2, u1, u2, m1, m2, collision_type):
     # Condition fulfilling elastic collision(for calculation of velocity)
-    if (int(x1) + r1) > (int(x2) - r2) and collision_type == 1:
+    if (int(x1) + r1) >= (int(x2) - r2) and collision_type == 1:
         v1 = (((m1 - m2)/(m1 + m2)) * u1) + (((2 * m2)/(m1 + m2)) * u2)
         v2 = (((2 * m1)/(m1 + m2)) * u1) + (((m2 - m1)/(m1 + m2)) * u2)
         # v1 = (u1 * (m1 - m2) + (2 * m2 * u2)) / (m1 + m2)
         # v2 = (u2 * (m2 - m1) + (2 * m1 * u1)) / (m1 + m2)
         return v1, v2
     # Condition fulfilling inelastic collision(for calculation of velocity)
-    elif (int(x1) + r1) > (int(x2) - r2) and collision_type == 2:
+    elif (int(x1) + r1) >= (int(x2) - r2) and collision_type == 2:
         v1 = v2 = ((m1 * u1) + (m2 * u2))/(m1 + m2)
         return v1, v2
     # Condition fulfilling elastic collision in the left boundary
-    elif (int(x1) - r1) < 10 and collision_type == 1:
+    elif (int(x1) - r1) <= 10 and collision_type == 1:
         v1 = -u1
         v2 = u2
         return v1, v2
     # Condition fulfilling inelastic collision in the left boundary
-    elif (int(x1) - r1) < 10 and collision_type == 2:
+    elif (int(x1) - r1) <= 10 and collision_type == 2:
         v1 = -u1
         v2 = -u2
         return v1, v2
     # Condition fulfilling elastic collision in the right boundary
-    elif (int(x2) + r2) > 1356 and collision_type == 1:
+    elif (int(x2) + r2) >= 1356 and collision_type == 1:
         v1 = u1
         v2 = -u2
         return v1, v2
     # Condition fulfilling inelastic collision in the right boundary
-    elif (int(x2) + r2) > 1356 and collision_type == 2:
+    elif (int(x2) + r2) >= 1356 and collision_type == 2:
         v1 = -u1
         v2 = -u2
         return v1, v2
@@ -88,9 +113,7 @@ def has_collided(x1, x2, r1, r2, u1, u2, m1, m2, collision_type):  # Function fo
 
 
 def obj_move(x1, x2, r1, r2, u1, u2, m1, m2, dt, collision_type):  # Function for moving the objects on the screen
-    v = has_collided(x1, x2, r1, r2, u1, u2, m1, m2, collision_type)  # Checking whether the objects have collided
-    v1 = v[0]
-    v2 = v[1]
+    v1, v2 = has_collided(x1, x2, r1, r2, u1, u2, m1, m2, collision_type)  # Checking whether the objects have collided
     x1 += v1 * dt
     x2 += v2 * dt
     return x1, x2, v1, v2
@@ -129,22 +152,47 @@ def main_program():  # Function for the main program loop
     r = size_obj(m1, m2)
     r1 = r[0]  # Retrieving the radius of the objects
     r2 = r[1]
-    dt = 0
+    dt = 0.1
     while not done:  # Infinite program loop
         screen.fill((255, 255, 255))
         border(screen)
-        # display_value()
+        value_mass1 = str(m1)  # Converting the value of mass to string
+        value_mass2 = str(m2)
+        temp_value1 = u1  # Assigning temporary variable for storing velocity in order to reverse the sign if negative
+        temp_value2 = u2
+        if u1 < 0:
+            temp_value1 = -u1
+        if u2 < 0:
+            temp_value2 = -u2
+        formatted_u1 = '{temp_value:0.3f}'.format(temp_value=temp_value1)  # For displaying only 3 digits after decimal
+        formatted_u2 = '{temp_value:0.3f}'.format(temp_value=temp_value2)
+        value_velocity1 = str(formatted_u1)  # Converting value of velocity to string
+        value_velocity2 = str(formatted_u2)
+        mass1 = DisplayDetails(screen, (960, 35), 'Mass of first object: ')
+        mass2 = DisplayDetails(screen, (960, 55), 'Mass of second object: ')
+        velocity1 = DisplayDetails(screen, (960, 75), 'Velocity of first object: ')
+        velocity2 = DisplayDetails(screen, (960, 95), 'Velocity of second object: ')
+        mass_value1 = DisplayValues(screen, (1260, 35), value_mass1)
+        mass_value2 = DisplayValues(screen, (1260, 55), value_mass2)
+        velocity_value1 = DisplayValues(screen, (1260, 75), value_velocity1)
+        velocity_value2 = DisplayValues(screen, (1260, 95), value_velocity2)
+        mass1.display_details()
+        mass2.display_details()
+        velocity1.display_details()
+        velocity2.display_details()
+        mass_value1.display_value()
+        mass_value2.display_value()
+        velocity_value1.display_value()
+        velocity_value2.display_value()
         event_handle()
         pygame.draw.circle(screen, (255, 0, 0), (int(x1), y1), r1, 0)
-        pygame.draw.circle(screen, (255, 0, 0), (int(x2), y2), r2, 0)
+        pygame.draw.circle(screen, (0, 255, 0), (int(x2), y2), r2, 0)
         x = obj_move(x1, x2, r1, r2, u1, u2, m1, m2, dt, collision_type)
         x1 = x[0]  # Retrieving the coordinate of the point where the body is
         x2 = x[1]
         u1 = x[2]  # Retrieving the velocity with which the body moves
         u2 = x[3]
-        dt += 0.0001
         pygame.display.flip()
-        pygame.display.update()
 
 
 def elastic_collision():
@@ -153,5 +201,5 @@ def elastic_collision():
     quit()
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     elastic_collision()
